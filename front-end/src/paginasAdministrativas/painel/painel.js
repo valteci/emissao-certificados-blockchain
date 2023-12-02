123424cd8c6713b3f3dd2678f3c8fe54629e6e90
valteci <valtecijunior@gmail.com>
2023-12-02 16:50:53 -0300

    side bar crud pronta

---
+++ front-end/src/paginasAdministrativas/painel/painel.js
@@ -0,0 +1,15 @@
+function redirecionarParaPaginaAluno() {
+    window.location.href = '../crud_aluno/crud_aluno.html';
+}
+
+function redirecionarParaPaginaCurso() {
+    window.location.href = '../crud_curso/crud_curso.html';
+}
+
+function redirecionarParaPaginaEmitirCertificado() {
+    window.location.href = '../emitirCertificado/emitirCertificado.html';
+}
+
+function redirecionarParaPaginaTurma() {
+    window.location.href = '../crud_turm/crud_turma.html';
+}
