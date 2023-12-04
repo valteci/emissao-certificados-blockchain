8310c41ecf2000e68a001bdd77d481a5190a9361
valteci <valtecijunior@gmail.com>
2023-12-04 16:04:24 -0300

    conclusao do crud curso

---
+++ front-end/src/paginasAdministrativas/crud_curso/crud_curso.js
@@ -0,0 +1,34 @@
+function opcaoCadastrar() {
+    document.getElementById('consultar').style.display = 'none';
+    document.getElementById('remover').style.display = 'none';
+    document.getElementById('alterar').style.display = 'none';
+    document.getElementById('cadastrar').style.display = 'block';
+}
+
+function opcaoConsulta() {
+    document.getElementById('consultar').style.display = 'block';
+    document.getElementById('remover').style.display = 'none';
+    document.getElementById('alterar').style.display = 'none';
+    document.getElementById('cadastrar').style.display = 'none';
+}
+
+function opcaoAlterar() {
+    document.getElementById('consultar').style.display = 'none';
+    document.getElementById('remover').style.display = 'none';
+    document.getElementById('alterar').style.display = 'block';
+    document.getElementById('cadastrar').style.display = 'none';
+}
+
+function opcaoRemover() {
+    document.getElementById('consultar').style.display = 'none';
+    document.getElementById('remover').style.display = 'block';
+    document.getElementById('alterar').style.display = 'none';
+    document.getElementById('cadastrar').style.display = 'none';
+}
+
+function main() {
+
+}
+
+
+main();
