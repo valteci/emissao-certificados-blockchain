ff4b56f58a8550d7ab65f21c06cd8a9c0b3b36c1
valteci <valtecijunior@gmail.com>
2023-12-10 00:02:03 -0300

    validação de certificados

--- front-end/src/certificados/certificados.js
+++ front-end/src/certificados/certificados.js
@@ -1,5 +1,41 @@
-async function verificarCertificado() {
-    
+async function verificarCertificado(evento) {
+    document.getElementById('invalido').style.display = 'none';
+    document.getElementById('valido').style.display = 'none';
+    evento.preventDefault();
+
+    try {
+        document.getElementById('loader').style.display = 'block';
+        document.getElementById('overlay').style.display = 'block';
+
+        const enderecoContrato = document.getElementById('inputEnderecoCertificado').value;
+        const resposta = await fetch(`http://localhost:3333/certificado/verify/${enderecoContrato}`);
+
+        if (!resposta.ok) {
+            const erro = await resposta.text();
+            alert('Erro: ' + JSON.parse(erro).message);
+            throw erro;
+        }
+
+        const dados = await resposta.json();
+
+        if (dados.isValid) {
+            document.getElementById('valido').style.display = 'block';
+            document.getElementById('link').setAttribute(
+                'href',
+                `https://sepolia.etherscan.io/address/${enderecoContrato}#readContract`)
+        }
+        else {
+            document.getElementById('invalido').style.display = 'block';
+        }
+
+        document.getElementById('loader').style.display = 'none';
+        document.getElementById('overlay').style.display = 'none';
+
+    } catch(erro)  {
+        console.log(erro);
+        document.getElementById('loader').style.display = 'none';
+        document.getElementById('overlay').style.display = 'none';
+    }
 }
 
 async function getPublicKey() {
@@ -19,7 +55,11 @@ function showPublicKey() {
         })
 }
 
-document.getElementById('formularioEnderecoContrato')
+function main() {
+    document.getElementById('formularioEnderecoContrato')
     .addEventListener('submit', verificarCertificado);
 
-showPublicKey();
+    showPublicKey();
+}
+
+main();
